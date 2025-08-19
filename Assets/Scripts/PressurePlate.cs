cb59d6299a472ca87fe9c31569d32cafc4d46775
Alan.Stewart@port.ac.uk <Alan.Stewart@port.ac.uk>
2025-08-19 10:06:55 +0100

    Scene switch delay - optimal approach (inc. coroutines)

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class PressurePlate : MonoBehaviour
 {
+    int loadDelay = 3;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("PP"))
         {
-            SceneManager.LoadScene(1);
+            StartCoroutine(LoadNextLevel());
         }
 
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene(currentSceneIndex+1);
     }
 }
