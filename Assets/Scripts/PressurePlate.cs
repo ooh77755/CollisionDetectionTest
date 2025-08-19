c91a2781a7b2cc4f3a01d7585cc15341961b6225
Alan.Stewart@port.ac.uk <Alan.Stewart@port.ac.uk>
2025-08-19 09:59:20 +0100

    Immediate scene load (no delay)

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PressurePlate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("PP"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
     }
 }
